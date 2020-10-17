0c13e7b7d0d8197778cfbcd60cfb2148ff6eeffb
NZMinus <developer.n.zaytsev@gmail.com>
2020-10-17 16:29:56 +0300

    feat(place form-dropdown)

--- app/src/pages/form-elements/form-elements.ts
+++ app/src/pages/form-elements/form-elements.ts
@@ -2,3 +2,7 @@ import "./form-elements.pug";
 
 // focus multiple components
 document.querySelector("#text-field-hover").focus();
+
+document.querySelector("#dropdown-expanded-plural").classList.add("menu-open");
+document.querySelector("#dropdown-expanded-apply").classList.add("menu-open");
+document.querySelector("#dropdown-expanded-clear-apply").classList.add("menu-open");
