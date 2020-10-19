5ed0641a5b3bca31ef604d3fa669f80227cd806c
NZMinus <developer.n.zaytsev@gmail.com>
2020-10-19 21:23:14 +0300

    fix(remove material icons into global)

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -96,6 +96,7 @@ class ResultOfTemplatesProcessing {
       this.entries[shortNameOfTemplate] = [
         "@babel/polyfill",
         `./pages/${shortNameOfTemplate}/${shortNameOfTemplate}.ts`,
+        "./utils/global/global.decl.ts",
         "./utils/normalizeCSS/normalize.decl.ts",
         "./components/thematic/main-theme.blocks/main-theme.scss",
       ];
