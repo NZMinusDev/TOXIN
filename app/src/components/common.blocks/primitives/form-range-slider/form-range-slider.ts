6b18b062625a6e9701fdb2fdc35006274585e03b
NZMinus <developer.n.zaytsev@gmail.com>
2020-10-19 18:31:11 +0300

    feat(add range-slider)

---
+++ app/src/components/common.blocks/primitives/form-range-slider/form-range-slider.ts
@@ -0,0 +1,36 @@
+import noUiSlider from "@library.blocks/primitives/form-range-slider/form-range-slider";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sliders = document.querySelectorAll(".form-range-slider__slider");
+
+  sliders.forEach((slider: HTMLDivElement) => {
+    const start = JSON.parse(slider.dataset.start);
+    const range = JSON.parse(slider.dataset.range);
+
+    noUiSlider.create(slider, {
+      start,
+      range,
+      connect: true, // Display colored bars between handles
+      step: 1,
+    });
+
+    const inputFrom = document.querySelector(`#${slider.id}-from`) as HTMLInputElement;
+    const inputTo = document.querySelector(`#${slider.id}-to`) as HTMLInputElement;
+    const result = document.querySelector(`#${slider.id}-result`) as HTMLOutputElement;
+
+    inputFrom.min = inputTo.min = range["min"];
+    inputFrom.max = inputTo.max = range["max"];
+
+    slider.noUiSlider.on("update", (values: Array<string>) => {
+      inputFrom.value = values[0];
+      inputTo.value = values[values.length - 1];
+      console.log(values[0]);
+      result.value =
+        parseInt(values[0]).toLocaleString() +
+        result.dataset.suffix +
+        " - " +
+        parseInt(values[values.length - 1]).toLocaleString() +
+        result.dataset.suffix;
+    });
+  });
+});
