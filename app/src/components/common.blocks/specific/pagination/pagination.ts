e5611c80345ad632b02579553e518a52a79e1ca9
NZMinus <developer.n.zaytsev@gmail.com>
2020-10-28 18:05:13 +0300

    fix(add common colors for components)

--- app/src/components/common.blocks/specific/pagination/pagination.ts
+++ app/src/components/common.blocks/specific/pagination/pagination.ts
@@ -3,7 +3,7 @@ import "./__item/pagination__item.scss";
 import "./__list/pagination__list.scss";
 import "./__item/_active/pagination__item_active.scss";
 import "./__item/_next/pagination__item_next.scss";
-import "./__item/previous/pagination__item_previous.scss";
+import "./__item/_previous/pagination__item_previous.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   const createPaginationItem = (
