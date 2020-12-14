1c9c40681c58ecf0de7ed8e58c5f422fca0f1a0b
NZMinus <developer.n.zaytsev@gmail.com>
2020-12-14 15:21:04 +0300

    feat(add filter features for some forms)

--- app/src/components/common.blocks/primitives/form-range-slider/form-range-slider.ts
+++ app/src/components/common.blocks/primitives/form-range-slider/form-range-slider.ts
@@ -1,35 +1,45 @@
+import { WindowHrefHelper } from "@utils/devTools/devTools";
+
 import noUiSlider from "@library.blocks/primitives/form-range-slider/form-range-slider";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const sliders = document.querySelectorAll(".form-range-slider__slider");
+export const sliderContainers = document.querySelectorAll(".form-range-slider");
 
-  sliders.forEach((slider: HTMLDivElement) => {
-    const start = JSON.parse(slider.dataset.start);
-    const range = JSON.parse(slider.dataset.range);
+sliderContainers.forEach((sliderContainer: HTMLDivElement) => {
+  const slider = sliderContainer.querySelector(".form-range-slider__slider") as HTMLDivElement;
+  const result = sliderContainer.querySelector(".form-range-slider__result") as HTMLOutputElement;
+  const inputFrom = sliderContainer.querySelector(`[name=${result.name}-from]`) as HTMLInputElement;
+  const inputTo = sliderContainer.querySelector(`[name=${result.name}-to]`) as HTMLInputElement;
 
-    noUiSlider.create(slider, {
-      start,
-      range,
-      connect: true, // Display colored bars between handles
-      step: 1,
-    });
-
-    const inputFrom = document.querySelector(`#${slider.id}-from`) as HTMLInputElement;
-    const inputTo = document.querySelector(`#${slider.id}-to`) as HTMLInputElement;
-    const result = document.querySelector(`#${slider.id}-result`) as HTMLOutputElement;
-
-    inputFrom.min = inputTo.min = range["min"];
-    inputFrom.max = inputTo.max = range["max"];
-
-    slider.noUiSlider.on("update", (values: Array<string>) => {
-      inputFrom.value = values[0];
-      inputTo.value = values[values.length - 1];
-      result.value =
-        parseInt(values[0]).toLocaleString() +
-        result.dataset.suffix +
-        " - " +
-        parseInt(values[values.length - 1]).toLocaleString() +
-        result.dataset.suffix;
-    });
+  noUiSlider.create(slider, {
+    start: [parseFloat(inputFrom.value), parseFloat(inputTo.value)],
+    range: { min: parseFloat(inputFrom.min), max: parseFloat(inputFrom.max) },
+    connect: true, // Display colored bars between handles
+    step: parseFloat(inputFrom.step),
   });
+
+  slider.noUiSlider.on("update", (values: Array<string>) => {
+    inputFrom.value = values[0];
+    inputTo.value = values[values.length - 1];
+    result.value =
+      parseInt(values[0]).toLocaleString() +
+      (result.dataset.suffix ? result.dataset.suffix : "") +
+      " - " +
+      parseInt(values[values.length - 1]).toLocaleString() +
+      (result.dataset.suffix ? result.dataset.suffix : "");
+  });
+
+  if (sliderContainer.getAttribute("isFilter")) {
+    slider.noUiSlider.on("change", (values: Array<string>) => {
+      WindowHrefHelper.addValues(
+        {
+          name: inputFrom.getAttribute("name"),
+          value: inputFrom.value,
+        },
+        {
+          name: inputTo.getAttribute("name"),
+          value: inputTo.value,
+        }
+      );
+    });
+  }
 });
