837d0767de6c63bfdf5cb65d9fe940b124de1ab3
NZMinus <developer.n.zaytsev@gmail.com>
2020-12-22 11:19:10 +0300

    feat(search room page)

--- app/src/pages/search-room/search-room.ts
+++ app/src/pages/search-room/search-room.ts
@@ -1,1 +1,16 @@
 import "./search-room.pug";
+import "./search-room.scss";
+
+// assets
+import "@contents/rooms/luxury/888.png";
+import "@contents/rooms/luxury/980.png";
+import "@contents/rooms/common/840.png";
+import "@contents/rooms/common/856.png";
+import "@contents/rooms/common/740.png";
+import "@contents/rooms/common/982.png";
+import "@contents/rooms/common/678.png";
+import "@contents/rooms/common/450.png";
+import "@contents/rooms/common/350.png";
+import "@contents/rooms/common/666.png";
+import "@contents/rooms/common/444.png";
+import "@contents/rooms/common/352.png";
