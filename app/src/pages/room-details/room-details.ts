1a8774bec7741d6776a561f9f04f72810dc87871
NZMinus <developer.n.zaytsev@gmail.com>
2020-12-26 16:07:28 +0300

    feat(room-details + donut-chart)

--- app/src/pages/room-details/room-details.ts
+++ app/src/pages/room-details/room-details.ts
@@ -1,1 +1,9 @@
 import "./room-details.pug";
+import "./room-details.scss";
+
+// assets
+import "@contents/personas/1/1.png";
+import "@contents/personas/2/2.png";
+import "@contents/rooms/luxury/888/888.png";
+import "@contents/rooms/luxury/888/888_2.png";
+import "@contents/rooms/luxury/888/888_3.png";
