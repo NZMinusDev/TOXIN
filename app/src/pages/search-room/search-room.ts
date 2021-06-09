9e7cf4a1864e8731bd2c08c70e3d349c353ce336
NZMinus <developer.n.zaytsev@gmail.com>
2021-06-09 11:06:29 +0300

    refac(load assets using required inside pug)

--- app/src/pages/search-room/search-room.ts
+++ app/src/pages/search-room/search-room.ts
@@ -1,16 +1,2 @@
-import "./search-room.pug";
-import "./search-room.scss";
-
-// assets
-import "@contents/rooms/luxury/888/888.png";
-import "@contents/rooms/luxury/980/980.png";
-import "@contents/rooms/common/840/840.png";
-import "@contents/rooms/common/856/856.png";
-import "@contents/rooms/common/740/740.png";
-import "@contents/rooms/common/982/982.png";
-import "@contents/rooms/common/678/678.png";
-import "@contents/rooms/common/450/450.png";
-import "@contents/rooms/common/350/350.png";
-import "@contents/rooms/common/666/666.png";
-import "@contents/rooms/common/444/444.png";
-import "@contents/rooms/common/352/352.png";
+import './search-room.pug';
+import './search-room.scss';
