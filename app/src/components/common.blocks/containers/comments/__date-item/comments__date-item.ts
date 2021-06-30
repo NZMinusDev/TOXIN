9c82be4c5c4ac42732a9c2239c2e9639075856df
NZMinus <developer.n.zaytsev@gmail.com>
2021-06-30 17:59:38 +0300

    fix(pixel perfect)

--- app/src/components/common.blocks/containers/comments/__date-item/comments__date-item.ts
+++
@@ -1,8 +0,0 @@
-import { diffDate, MS_IN_DAY } from '@utils/devTools/scripts/DateHelper';
-
-document.querySelectorAll('.comments__date-item time').forEach((dateHTMLItem) => {
-  // eslint-disable-next-line no-param-reassign
-  dateHTMLItem.textContent = `${Math.abs(
-    diffDate(new Date(), new Date(dateHTMLItem.getAttribute('datetime') as string)) / MS_IN_DAY
-  ).toFixed(0)} дней назад`;
-});
