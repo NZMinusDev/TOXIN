615ece1b13242321f83f79d41d79081ae5ce09e8
NZMinus <developer.n.zaytsev@gmail.com>
2021-07-07 15:46:09 +0300

    fix(make slider works)

--- app/src/components/common.blocks/primitives/form-range-slider/form-range-slider.ts
+++ app/src/components/common.blocks/primitives/form-range-slider/form-range-slider.ts
@@ -1,45 +1,64 @@
-import { WindowHrefHelper } from "@utils/devTools/devTools";
+import noUiSlider from '@library.blocks/primitives/form-range-slider/form-range-slider';
 
-import noUiSlider from "@library.blocks/primitives/form-range-slider/form-range-slider";
+interface HTMLDivElementWithSlider extends HTMLDivElement {
+  // eslint-disable-next-line no-use-before-define
+  noUiSlider: noUiSlider;
+}
 
-export const sliderContainers = document.querySelectorAll(".form-range-slider");
+const sliderContainers = document.querySelectorAll('.form-range-slider') as NodeListOf<
+  HTMLDivElement
+>;
 
-sliderContainers.forEach((sliderContainer: HTMLDivElement) => {
-  const slider = sliderContainer.querySelector(".form-range-slider__slider") as HTMLDivElement;
-  const result = sliderContainer.querySelector(".form-range-slider__result") as HTMLOutputElement;
-  const inputFrom = sliderContainer.querySelector(`[name=${result.name}-from]`) as HTMLInputElement;
-  const inputTo = sliderContainer.querySelector(`[name=${result.name}-to]`) as HTMLInputElement;
+const parseDOM = (sliderContainer: HTMLDivElement) => {
+  const slider = sliderContainer.querySelector(
+    '.form-range-slider__slider'
+  ) as HTMLDivElementWithSlider;
+  const result = sliderContainer.querySelector('.form-range-slider__result') as HTMLOutputElement;
+  const inputFrom = sliderContainer.querySelector(`[name=${result.name}-0]`) as HTMLInputElement;
+  const inputTo = sliderContainer.querySelector(`[name=${result.name}-1]`) as HTMLInputElement;
 
-  noUiSlider.create(slider, {
-    start: [parseFloat(inputFrom.value), parseFloat(inputTo.value)],
-    range: { min: parseFloat(inputFrom.min), max: parseFloat(inputFrom.max) },
-    connect: true, // Display colored bars between handles
-    step: parseFloat(inputFrom.step),
+  return { slider, result, inputFrom, inputTo };
+};
+
+sliderContainers.forEach((sliderContainer) => {
+  const DOM = parseDOM(sliderContainer);
+
+  noUiSlider.create(DOM.slider, {
+    start: [DOM.inputFrom.value, DOM.inputTo.value],
+    range: { min: Number(DOM.inputFrom.min), max: Number(DOM.inputFrom.max) },
+    connect: true,
   });
 
-  slider.noUiSlider.on("update", (values: Array<string>) => {
-    inputFrom.value = values[0];
-    inputTo.value = values[values.length - 1];
-    result.value =
-      parseInt(values[0]).toLocaleString() +
-      "₽" +
-      " - " +
-      parseInt(values[values.length - 1]).toLocaleString() +
-      "₽";
-  });
+  const onUpdate = (values: Array<string>) => {
+    const [valueFrom, valueTo] = values;
 
-  if (sliderContainer.getAttribute("isFilter")) {
-    slider.noUiSlider.on("change", (values: Array<string>) => {
-      WindowHrefHelper.addValues(
-        {
-          name: inputFrom.getAttribute("name"),
-          value: inputFrom.value,
-        },
-        {
-          name: inputTo.getAttribute("name"),
-          value: inputTo.value,
-        }
-      );
-    });
-  }
+    DOM.inputFrom.value = valueFrom;
+    DOM.inputTo.value = valueTo;
+    DOM.result.value = `${parseInt(valueFrom, 10).toLocaleString()}₽ - ${parseInt(
+      valueTo,
+      10
+    ).toLocaleString()}₽`;
+  };
+
+  const onChange = (values: Array<string>, handle: number) => {
+    switch (handle) {
+      case 0: {
+        DOM.inputFrom.dispatchEvent(new Event('change'));
+
+        break;
+      }
+      case 1: {
+        DOM.inputTo.dispatchEvent(new Event('change'));
+
+        break;
+      }
+
+      // no default
+    }
+  };
+
+  DOM.slider.noUiSlider.on('update', onUpdate);
+  DOM.slider.noUiSlider.on('change', onChange);
 });
+
+export { sliderContainers as default };
