cb6f11a5c73313fdc253f078f173b6c1b295c13a
NZMinus <developer.n.zaytsev@gmail.com>
2021-07-10 14:58:18 +0300

    refac(minor useless code removing)

--- app/src/pages/form-elements/form-elements.ts
+++ app/src/pages/form-elements/form-elements.ts
@@ -5,9 +5,6 @@ import './form-elements.scss';
 // components
 import itemQuantityLists from '@common.blocks/primitives/form-dropdown/__item-quantity-list/form-dropdown__item-quantity-list';
 
-// focus components
-(document.querySelector('#text') as HTMLElement).focus();
-
 itemQuantityLists.forEach((itemQuantityList) => {
   const shouldBeOpened =
     itemQuantityList.element.closest('.form-elements-layout__dropdown-expanded-plural') ||
