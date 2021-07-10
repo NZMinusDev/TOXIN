adb97c9a4ab74ceef21759409ccc31ec39705d5e
NZMinus <developer.n.zaytsev@gmail.com>
2021-07-10 11:58:39 +0300

    refac(webpack: use @babel plugins from preset)

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -402,6 +402,7 @@ const jsLoaders = (extraPreset) => {
     plugins: [
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
     ],
     cacheDirectory: './app/cache/webpack__babel',
   };
