0704e3a5b160ef52bf4e1ca9f5663204ad954746
NZMinus <developer.n.zaytsev@gmail.com>
2021-07-16 15:56:45 +0300

    fix(iqList_open-method_folded: close if opened )

--- app/src/components/common.blocks/primitives/form-dropdown/__item-quantity-list/_opening-method/form-dropdown__item-quantity-list_opening-method_folded.ts
+++ app/src/components/common.blocks/primitives/form-dropdown/__item-quantity-list/_opening-method/form-dropdown__item-quantity-list_opening-method_folded.ts
@@ -20,7 +20,10 @@ class FormDropdownItemQuantityListFoldedOpeningMethodModifier extends FormDropdo
       const target = event.target as HTMLElement;
       const dropdownElement = this.component.element.closest('.js-form-dropdown') as HTMLElement;
 
-      if (!dropdownElement.contains(target)) {
+      if (
+        !dropdownElement.contains(target) &&
+        this.component.element.classList.contains('menu-open')
+      ) {
         this.component.close();
       }
     },
