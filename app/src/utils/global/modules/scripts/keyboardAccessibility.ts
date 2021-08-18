b94fa65866c1e459b2b58d904cca92aece009894
NZMinus <developer.n.zaytsev@gmail.com>
2021-08-18 13:22:54 +0300

    fix(keyboardAccess: don't add click for <button>)

--- app/src/utils/global/modules/scripts/keyboardAccessibility.ts
+++ app/src/utils/global/modules/scripts/keyboardAccessibility.ts
@@ -1,7 +1,9 @@
 const onKeyDown = (event) => {
   const isActive = !event.repeat && document.activeElement !== null;
+  const clickIsEmulated = document.activeElement?.tagName === 'BUTTON';
+  const shouldEmulate = !clickIsEmulated && isActive && event.code === 'Enter';
 
-  if (isActive && event.code === 'Enter') {
+  if (shouldEmulate) {
     (document.activeElement as HTMLElement).click();
   }
 };
