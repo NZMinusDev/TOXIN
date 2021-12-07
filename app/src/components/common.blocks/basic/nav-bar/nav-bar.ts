9ab42ba1f1f2064576c4c4311714c752f396df19
NZMinus <developer.n.zaytsev@gmail.com>
2021-12-07 19:03:32 +0300

    refac(nav-bar: add burger mode)

--- app/src/components/common.blocks/basic/nav-bar/nav-bar.ts
+++ app/src/components/common.blocks/basic/nav-bar/nav-bar.ts
@@ -4,9 +4,140 @@ import BEMComponent, {
 
 import navBarElements, { NavBarElement } from './nav-bar-elements';
 
+type NavBarDOM = {
+  burger: HTMLButtonElement;
+  body: HTMLDivElement;
+};
+
+type NavBarState = {
+  isMobile: boolean;
+};
+
 type NavBarCustomEvents = {};
 
-class NavBar extends BEMComponent<NavBarElement, NavBarCustomEvents> {}
+const activeBurgerClass = 'nav-bar__burger_active';
+
+class NavBar extends BEMComponent<NavBarElement, NavBarCustomEvents> {
+  protected readonly _DOM: Readonly<NavBarDOM>;
+
+  protected readonly _state: NavBarState;
+
+  constructor(navBarElement: NavBarElement) {
+    super(navBarElement);
+
+    this._DOM = this._initDOM();
+
+    this._state = NavBar._initState();
+
+    this._bindBurgerListeners()._bindWindowListeners();
+
+    this._initDisplay();
+  }
+
+  protected _initDOM() {
+    const { element } = this;
+
+    const burger = element.querySelector(
+      '.js-nav-bar__burger'
+    ) as NavBarDOM['burger'];
+    const body = element.querySelector(
+      '.js-nav-bar__body'
+    ) as NavBarDOM['body'];
+
+    return { burger, body };
+  }
+
+  protected static _initState() {
+    const isMobile = NavBar.matchIsMobile();
+
+    return { isMobile };
+  }
+
+  protected _bindBurgerListeners() {
+    const { burger } = this._DOM;
+
+    burger.addEventListener(
+      'click',
+      this._burgerEventListenerObject.handleBurgerClick
+    );
+
+    return this;
+  }
+
+  protected _burgerEventListenerObject = {
+    handleBurgerClick: () => {
+      this._toggleBody();
+    },
+  };
+
+  protected _bindWindowListeners() {
+    window.addEventListener(
+      'resize',
+      this._windowEventListenerObject.handleWindowResize
+    );
+
+    return this;
+  }
+
+  protected _windowEventListenerObject = {
+    handleWindowResize: () => {
+      const { isMobile } = this._state;
+      const newIsMobile = NavBar.matchIsMobile();
+
+      if (isMobile !== newIsMobile && newIsMobile) {
+        const timeForRerendering = 500;
+
+        this._turnOffSTransition();
+        this._toggleBody(false);
+        setTimeout(() => {
+          this._turnOnSTransition();
+        }, timeForRerendering);
+      }
+
+      this._state.isMobile = newIsMobile;
+    },
+  };
+
+  protected _initDisplay() {
+    this._turnOnSTransition();
+
+    return this;
+  }
+
+  protected static matchIsMobile() {
+    return window.matchMedia('(max-width: 992px)').matches;
+  }
+
+  protected _turnOnSTransition() {
+    const { body } = this._DOM;
+
+    body.classList.add('nav-bar__body_transition_active');
+
+    return this;
+  }
+
+  protected _turnOffSTransition() {
+    const { body } = this._DOM;
+
+    body.classList.remove('nav-bar__body_transition_active');
+
+    return this;
+  }
+
+  protected _toggleBody(forceOpen?: boolean) {
+    const { burger } = this._DOM;
+
+    if (forceOpen === false || burger.classList.contains(activeBurgerClass)) {
+      burger.classList.remove(activeBurgerClass);
+      burger.ariaExpanded = 'false';
+    } else if (forceOpen === true || forceOpen === undefined) {
+      burger.classList.add(activeBurgerClass);
+      burger.ariaExpanded = 'true';
+    }
+
+    return this;
+  }
+}
 
 type NavBarElementWithComponent = HTMLElementWithComponent<
   NavBarElement,
